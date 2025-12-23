5af4bb838c8605390057fa82464d13b58fa7010d
unknown <oluyaratosin123@gmail.com>
2025-12-23 15:34:18 +0100

    recent update

--- packages/server/config/settings/production.py
+++ packages/server/config/settings/production.py
@@ -1,7 +1,16 @@
 from .base import *
+import dj_database_url
+import os
 
 DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS", ".onrender.com"
 ).split(",")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
+}
